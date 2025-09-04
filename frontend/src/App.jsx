4031d80375a7dd81e4047cf383fb3b1debd84fef
twin-25 <tjssg678@gmail.com>
2025-09-03 20:10:07 -0400

    setup components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-
+import './assets/css/style.css';
+import Header from './componets/Header';
+import { Main } from './componets/Main';
+import Footer from './componets/Footer';
 
 function App() {
   
 
   return (
     <>
+    <Header />
+    <Main />
+    <Footer />
     </>
   )
 }
-
 export default App
---
+++ frontend/src/componets/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <nav className='navbar container'>
+        <a 
+        className='navbar-brand text-light' href=''
+        >Stock Prediction Portal
+        </a>
+      </nav>
+    </>
+  )
+}
+
+export default Header
